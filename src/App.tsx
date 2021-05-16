f520656c05013016e2d132c32001f5bfe18a7635
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-16 17:02:14 +0900

    [modify] make and use customHook!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  // 他のファイルでuseAllUsers()を使っても、stateは競合しない！
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div className="App">
